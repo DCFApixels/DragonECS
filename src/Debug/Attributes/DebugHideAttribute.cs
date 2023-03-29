3787f2fc38f2076f0676ae776e47d11eac91fc80
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-03-29 15:45:55 +0800

    Add DebugHide attribute

---
+++ src/Debug/Attributes/DebugHideAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace DCFApixels.DragonECS
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = true)]
+    public sealed class DebugHideAttribute : Attribute { }
+}
