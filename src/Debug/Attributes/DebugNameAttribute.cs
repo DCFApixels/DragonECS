9bc1eb42ab1bd01185cd74bd70353b30fecbb80b
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-04-03 06:18:07 +0800

    add DebugNameAttribute

---
+++ src/Debug/Attributes/DebugNameAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DCFApixels.DragonECS
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = true)]
+    public sealed class DebugNameAttribute : Attribute
+    {
+        public readonly string name;
+        public DebugNameAttribute(string name)
+        {
+            this.name = name;
+        }
+    }
+}
