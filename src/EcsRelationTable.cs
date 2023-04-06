b565510a69e304a3a7955b520958cf8fea27bae3
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-04-06 11:50:31 +0800

    fix changes

---
+++ src/EcsRelationTable.cs
@@ -0,0 +1,57 @@
+﻿using DCFApixels.DragonECS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditorInternal;
+
+namespace DCFApixels.Assets.DragonECS.src
+{
+    public class EcsRelationTable
+    {
+        public readonly IEcsWorld leftWorld;
+        public readonly IEcsWorld rightWorld;
+
+
+        private int[] _relationEntities; //dense
+        private int[] _leftMapping;
+        private int[] _rgihtMapping;
+
+        private int _relationsCount;
+
+
+        #region Properties
+        public int RelationsCount
+        {
+            get => _relationsCount;
+        }
+        #endregion
+
+
+        internal EcsRelationTable(IEcsWorld leftWorld, IEcsWorld rightWorld)
+        {
+            this.leftWorld = leftWorld;
+            this.rightWorld = rightWorld;
+
+            _relationEntities = new int[512];
+            _leftMapping = new int[512];
+            _rgihtMapping = new int[512];
+
+            _relationsCount = 0;
+        }
+
+        public void AddRelation(int leftEnttiyID, int rightEntityID)
+        {
+
+        }
+        public void RemoveRelationLeft(int entityID)
+        {
+
+        }
+        public void RemoveRelationRight(int entityID)
+        {
+
+        }
+    }
+}
---
+++ src/Interfaces/IEcsEntityComponentTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCFApixels.DragonECS
+{
+    public interface IEcsEntityComponentTable
+    {
+        #region Properties
+        public bool IsEmpty { get; }
+        public Type ArchetypeType { get; }
+        //public int ID { get; }
+        public int EntitesCount { get; }
+        public int EntitesCapacity { get; }
+        #endregion
+
+        #region GetterMethods
+        public ReadOnlySpan<IEcsPool> GetAllPools();
+
+        #endregion
+
+        #region Methods
+        public EcsPool<T> GetPool<T>() where T : struct;
+        public EcsPool<T> UncheckedGetPool<T>() where T : struct;
+
+        public EcsFilter Entities<TComponent>() where TComponent : struct;
+        public EcsFilter Filter<TInc>() where TInc : struct, IInc;
+        public EcsFilter Filter<TInc, TExc>() where TInc : struct, IInc where TExc : struct, IExc;
+
+        public ent NewEntity();
+        public void DelEntity(ent entity);
+        public bool EntityIsAlive(int entityID, short gen);
+        public ent GetEntity(int entityID);
+        public void Destroy();
+
+        public bool IsMaskCompatible<TInc>(int entity) where TInc : struct, IInc;
+        public bool IsMaskCompatible<TInc, TExc>(int entity) where TInc : struct, IInc where TExc : struct, IExc;
+        public bool IsMaskCompatible(EcsMask mask, int entity);
+        public bool IsMaskCompatibleWithout(EcsMask mask, int entity, int otherPoolID);
+
+        internal void OnEntityComponentAdded(int entityID, int changedPoolID);
+        internal void OnEntityComponentRemoved(int entityID, int changedPoolID);
+
+        public int GetComponentID<T>();
+        internal void RegisterGroup(EcsGroup group);
+        #endregion
+    }
+}
