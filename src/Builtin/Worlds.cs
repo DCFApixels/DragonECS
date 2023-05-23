0e793f70ec38fbfd2c24f03931ffdbe360d33dc5
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-05-23 16:00:27 +0800

    fix builtin worlds

--- src/Builtin/Worlds.cs
+++ src/Builtin/Worlds.cs
@@ -1,23 +1,5 @@
 ﻿namespace DCFApixels.DragonECS
 {
-    public sealed class EcsDefaultWorld : EcsWorld<EcsDefaultWorld>
-    {
-        public EcsDefaultWorld() : base(null) { }
-        public EcsDefaultWorld(EcsPipeline pipeline) : base(pipeline) { }
-    }
-    public sealed class EcsPhysicsEventsWorld : EcsWorld<EcsPhysicsEventsWorld>
-    {
-        public EcsPhysicsEventsWorld() : base(null) { }
-        public EcsPhysicsEventsWorld(EcsPipeline pipeline) : base(pipeline) { }
-    }
-    public sealed class EcsEventWorld : EcsWorld<EcsEventWorld>
-    {
-        public EcsEventWorld() : base(null) { }
-        public EcsEventWorld(EcsPipeline pipeline) : base(pipeline) { }
-    }
-    public sealed class EcsUIWorld : EcsWorld<EcsUIWorld>
-    {
-        public EcsUIWorld() : base(null) { }
-        public EcsUIWorld(EcsPipeline pipeline) : base(pipeline) { }
-    }
+    public sealed class EcsDefaultWorld : EcsWorld<EcsDefaultWorld> { }
+    public sealed class EcsEventWorld : EcsWorld<EcsEventWorld> { }
 }
