bdecd86e0050431ebaef9176e026bccf9e89d0b9
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-03-08 20:25:42 +0800

    fix

--- src/EcsWorld.cs
+++ src/EcsWorld.cs
@@ -360,7 +360,8 @@ namespace DCFApixels.DragonECS
             int delCount = 0;
             foreach (var e in Entities)
             {
-                if (_entities[e].componentsCount <= 0)
+                ref var ent = ref _entities[e];
+                if (ent.componentsCount <= 0 && ent.isUsed)
                 {
                     DelEntity(e);
                     delCount++;
