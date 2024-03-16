f1c8b6e39facb9c5c58ff5f6a7cadfbc446cc9c8
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-03-17 05:22:33 +0800

    Create UncheckedCoreUtility.cs

---
+++ src/Utils/UncheckedCoreUtility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace DCFApixels.DragonECS.UncheckedCore
+{
+    public static class UncheckedCoreUtility
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static EcsSpan CreateSpan(short worldID, int[] entitesArray, int startIndex, int length)
+        {
+            return new EcsSpan(worldID, entitesArray, startIndex, length);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static EcsSpan CreateSpan(short worldID, int[] entitesArray, int length)
+        {
+            return new EcsSpan(worldID, entitesArray, length);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static EcsSpan CreateSpan(short worldID, int[] entitesArray)
+        {
+            return new EcsSpan(worldID, entitesArray);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static EcsSpan CreateEmptySpan(short worldID)
+        {
+            return new EcsSpan(worldID, Array.Empty<int>());
+        }
+        public static bool CheckSpanValideDebug(EcsSpan span)
+        {
+            HashSet<int> set = new HashSet<int>(span.Count);
+            foreach (var e in span)
+            {
+                if (set.Add(e) == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
