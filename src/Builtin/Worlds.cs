44c49b987aeb356cf7d3816d9bebdf4c0cf9a5ca
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-04-09 00:19:43 +0800

    add debugger proxy

--- src/Builtin/Worlds.cs
+++ src/Builtin/Worlds.cs
@@ -1,10 +1,14 @@
-﻿namespace DCFApixels.DragonECS
+﻿using System.Diagnostics;
+
+namespace DCFApixels.DragonECS
 {
+    [DebuggerTypeProxy(typeof(DebuggerProxy))]
     public sealed class EcsDefaultWorld : EcsWorld
     {
         public EcsDefaultWorld(EcsWorldConfig config, short worldID = -1) : base(config, worldID) { }
         public EcsDefaultWorld(IConfigContainer configs = null, short worldID = -1) : base(configs, worldID) { }
     }
+    [DebuggerTypeProxy(typeof(DebuggerProxy))]
     public sealed class EcsEventWorld : EcsWorld
     {
         public EcsEventWorld(EcsWorldConfig config, short worldID = -1) : base(config, worldID) { }
