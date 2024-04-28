2ad70a48bc3dcf1c5a3c32b84a139ba512ed2c9d
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-04-28 22:22:06 +0800

    add Il2CppSetOptionAttribute

--- src/Consts.cs
+++ src/Consts.cs
@@ -72,3 +72,24 @@
 //#if UNITY_2020_3_OR_NEWER
 //        [UnityEngine.Scripting.RequireDerived, UnityEngine.Scripting.Preserve]
 //#endif
+
+
+
+
+#if ENABLE_IL2CPP
+// Unity IL2CPP performance optimization attribute.
+namespace Unity.IL2CPP.CompilerServices {
+    enum Option {
+        NullChecks = 1,
+        ArrayBoundsChecks = 2
+    }
+
+    [AttributeUsage (AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    class Il2CppSetOptionAttribute : Attribute {
+        public Option Option { get; private set; }
+        public object Value { get; private set; }
+
+        public Il2CppSetOptionAttribute (Option option, object value) { Option = option; Value = value; }
+    }
+}
+#endif
