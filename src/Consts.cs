0252f9e0a5fe4fde8de7556def1f2ba58486ff8a
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-04-29 17:42:24 +0800

    fix

--- src/Consts.cs
+++ src/Consts.cs
@@ -78,17 +78,19 @@
 
 #if ENABLE_IL2CPP
 // Unity IL2CPP performance optimization attribute.
-namespace Unity.IL2CPP.CompilerServices {
-    enum Option {
+namespace Unity.IL2CPP.CompilerServices 
+{
+    using System;
+    enum Option 
+    {
         NullChecks = 1,
         ArrayBoundsChecks = 2
     }
-
     [AttributeUsage (AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
-    class Il2CppSetOptionAttribute : Attribute {
+    class Il2CppSetOptionAttribute : Attribute 
+    {
         public Option Option { get; private set; }
         public object Value { get; private set; }
-
         public Il2CppSetOptionAttribute (Option option, object value) { Option = option; Value = value; }
     }
 }
