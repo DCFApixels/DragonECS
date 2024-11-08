ed698fbbd8fb340afd683ac34fc2507a39eb1034
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-11-08 17:20:05 +0800

    add EntitesMatrix

--- src/Utils/UncheckedCoreUtility.cs
+++ src/Utils/UncheckedCoreUtility.cs
@@ -45,4 +45,42 @@ namespace DCFApixels.DragonECS.UncheckedCore
             return true;
         }
     }
+
+    public readonly struct EntitesMatrix
+    {
+        private readonly EcsWorld _world;
+        public EntitesMatrix(EcsWorld world)
+        {
+            _world = world;
+        }
+        public int PoolsCount
+        {
+            get { return _world.PoolsCount; }
+        }
+        public int EntitesCount
+        {
+            get { return _world.Capacity; }
+        }
+        public int GetEntityComponentsCount(int entityID)
+        {
+            return _world.GetComponentsCount(entityID);
+        }
+        public int GetEntityGen(int entityID)
+        {
+            return _world.GetGen(entityID);
+        }
+        public bool IsEntityUsed(int entityID)
+        {
+            return _world.IsUsed(entityID);
+        }
+        public bool this[int entityID, int poolID]
+        {
+            get
+            {
+                int entityStartChunkIndex = entityID << _world._entityComponentMaskLengthBitShift;
+                var chunkInfo = EcsMaskChunck.FromID(poolID);
+                return (_world._entityComponentMasks[entityStartChunkIndex + chunkInfo.chunkIndex] & chunkInfo.mask) != 0;
+            }
+        }
+    }
 }
