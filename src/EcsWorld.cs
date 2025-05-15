ea311afba208707050f719e8512aaa3f40330177
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-05-15 11:25:18 +0800

    fix

--- src/EcsWorld.cs
+++ src/EcsWorld.cs
@@ -133,11 +133,7 @@ namespace DCFApixels.DragonECS
         public EcsSpan Entities
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get
-            {
-                ReleaseDelEntityBufferAll();
-                return GetCurrentEntities_Internal();
-            }
+            get { return ToSpan(); }
         }
         public int PoolsCount
         {
@@ -459,7 +455,7 @@ namespace DCFApixels.DragonECS
             {
                 ReleaseDelEntityBufferAll();
             }
-            return _entityDispenser.UsedToEcsSpan(ID);
+            return GetCurrentEntities_Internal();
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public unsafe entlong GetEntityLong(int entityID)
