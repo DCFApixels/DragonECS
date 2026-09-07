53c53bdb13cc0c88a77893b078159205355d8419
ByteWarsJedi <99481254+DCFApixels@users.noreply.github.com>
2026-09-07 17:16:09 +0800

    Update MetaTagsAttribute.cs

--- src/DebugUtils/MetaAttributes/MetaTagsAttribute.cs
+++ src/DebugUtils/MetaAttributes/MetaTagsAttribute.cs
@@ -34,7 +34,7 @@ namespace DCFApixels.DragonECS
         public MetaTagsAttribute(string tag0, string tag1, string tag2, string tag3, string tag4, string tag5) : this($"{tag0},{tag1},{tag2},{tag3},{tag4},{tag5}") { }
         public MetaTagsAttribute(string tag0, string tag1, string tag2, string tag3, string tag4, string tag5, string tag6) : this($"{tag0},{tag1},{tag2},{tag3},{tag4},{tag5},{tag6}") { }
         public MetaTagsAttribute(string tag0, string tag1, string tag2, string tag3, string tag4, string tag5, string tag6, string tag7) : this($"{tag0},{tag1},{tag2},{tag3},{tag4},{tag5},{tag6},{tag7}") { }
-        public MetaTagsAttribute(params string[] tags) : this(string.Join(SEPARATOR, tags)) { }
+        public MetaTagsAttribute(params string[] tags) : this(tags == null ? null : string.Join(SEPARATOR, tags)) { }
     }
     public readonly ref struct MetaTags
     {
