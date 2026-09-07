af9b377892e24a311bc22b66dc9557992e922265
ByteWarsJedi <99481254+DCFApixels@users.noreply.github.com>
2026-09-07 17:42:07 +0800

    Update EcsPipelineTemplate.cs

--- src/Utils/EcsPipelineTemplate.cs
+++ src/Utils/EcsPipelineTemplate.cs
@@ -21,7 +21,8 @@ namespace DCFApixels.DragonECS
         [DataMember] public Record[] records;
         void IEcsModule.Import(EcsPipeline.Builder b)
         {
-            b.Layers.MergeWith(layers);
+            if (layers != null) { b.Layers.MergeWith(layers); }
+            if (records == null) { return; }
             foreach (var s in records)
             {
                 if (s.target == null) { continue; }
